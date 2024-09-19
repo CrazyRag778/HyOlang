ab3ebac180aae6f45f1807e98233b603c1de7dd4
Dibyojit Datta <dattadibyojit@gmail.com>
2024-09-19 20:37:09 +0530

    Add Striping for Code Argument
    Previously, the compilier had to do some basic string search for getting the parameters, but now it has to just check for the arguments.

--- compile.py
+++ compile.py
@@ -18,25 +18,30 @@ for i in ccode:
     if len(i[0]) > 0 and i[0][0] == " ":
         i[0] = i[0].lstrip()
 
-    if (i[0]=="rawPRINT "):
-        print(i[1][2:-1], end="")
-    elif (i[0]=="PRINT "):
-        print(i[1][2:-1])
-    elif (i[0]=="fPRINT "):
-        print(eval(i[1][1:]), end="")
-    elif (i[0]=="LOOpRINT "):
+    tempI = []
+    for cO in i:
+        tempI.append(cO.strip())
+    i = tempI
+
+    if (i[0]=="rawPRINT"):
+        print(i[1], end="")
+    elif (i[0]=="PRINT"):
+        print(i[1])
+    elif (i[0]=="fPRINT"):
+        print(eval(i[1]), end="")
+    elif (i[0]=="LOOpRINT"):
         for iIndexEl in range(1, len(i)):
-            print(i[iIndexEl][2:-2], end="")
-    elif (i[0]=="TYPE "):
+            print(i[iIndexEl], end="")
+    elif (i[0]=="TYPE"):
         print(f"<OBJECT {type(ast.literal_eval(i[1])).__name__}>", end="")
-    elif (i[0]=="INPUT "):
-        exec(f"{str(i[2][1:])}=input('{i[1][2:-2]}')")
-    elif (i[0]=="showVAR "):
-        print(eval(i[1][1:]), end="")
-    elif (i[0]=="take "):
-        exec(f"{i[1][1:-1]} = {i[2][1:]}")
-    elif (i[0]=="@ALLOW "):
-        allowed_module[str(i[1][1:])] = 1
+    elif (i[0]=="INPU"):
+        exec(f"{str(i[2])}=input('{i[1]}')")
+    elif (i[0]=="showVAR"):
+        print(eval(i[1]), end="")
+    elif (i[0]=="take"):
+        exec(f"{i[1]} = {i[2]}")
+    elif (i[0]=="@ALLOW"):
+        allowed_module[str(i[1])] = 1
     elif (i[0]=="END"):
         exit()
     
@@ -44,13 +49,13 @@ for i in ccode:
     # FILE module
     elif (i[0][0:5]=="FILE."):
         if (allowed_module["FILE"]==1):
-            if (i[0][5:]=="ACCESS "):
-                exec(f"{i[3][1:]} = open('{i[1][1:-1]}', '{i[2][1:-1].lower()}')")
+            if (i[0][5:]=="ACCESS"):
+                exec(f"{i[3]} = open('{i[1]}', '{i[2].lower()}')")
             elif (i[0][5:]=="CLOSE "):
-                exec(f"{i[1][1:]}.close()")
+                exec(f"{i[1]}.close()")
             elif (i[0][5:]=="CREATE "):
-                exec(f"{i[2][1:]} = open('{i[1][1:-1]}', 'x')")
+                exec(f"{i[2]} = open('{i[1]}', 'x')")
             elif (i[0][5:]=="DELETE "):
-                exec(f"os.remove({i[1][1:]}.name)")        
+                exec(f"os.remove({i[1]}.name)")        
             elif (i[0][5:]=="WRITE "):
-                exec(f"{i[1][1:-1]}.write({i[2][1:]})")    
+                exec(f"{i[1]}.write({i[2]})")    
