d1a2ba2e3f6c2c915209bb4c222b405e9ab280c9
Dibyojit Datta <dattadibyojit@gmail.com>
2024-09-19 20:42:17 +0530

    Replace varibale i with parseArg
    Changed the name for variable 'i' to 'parseArg' and 'tempI' to 'tempParsArg' hence.

--- compile.py
+++ compile.py
@@ -11,51 +11,51 @@ allowed_module = {
     "FILE": 0,
 }
 
-for i in ccode:
+for parsArg in ccode:
     # Splits the syntax into function and arguments
-    i = i.split("-")
+    parsArg = parsArg.split("-")
     # Removes leading whitespace
-    if len(i[0]) > 0 and i[0][0] == " ":
-        i[0] = i[0].lstrip()
+    if len(parsArg[0]) > 0 and parsArg[0][0] == " ":
+        parsArg[0] = parsArg[0].lstrip()
 
-    tempI = []
-    for cO in i:
-        tempI.append(cO.strip())
-    i = tempI
+    tempParsArg = []
+    for cO in parsArg:
+        tempParsArg.append(cO.strip())
+    parsArg = tempParsArg
 
-    if (i[0]=="rawPRINT"):
-        print(i[1], end="")
-    elif (i[0]=="PRINT"):
-        print(i[1])
-    elif (i[0]=="fPRINT"):
-        print(eval(i[1]), end="")
-    elif (i[0]=="LOOpRINT"):
-        for iIndexEl in range(1, len(i)):
-            print(i[iIndexEl], end="")
-    elif (i[0]=="TYPE"):
-        print(f"<OBJECT {type(ast.literal_eval(i[1])).__name__}>", end="")
-    elif (i[0]=="INPU"):
-        exec(f"{str(i[2])}=input('{i[1]}')")
-    elif (i[0]=="showVAR"):
-        print(eval(i[1]), end="")
-    elif (i[0]=="take"):
-        exec(f"{i[1]} = {i[2]}")
-    elif (i[0]=="@ALLOW"):
-        allowed_module[str(i[1])] = 1
-    elif (i[0]=="END"):
+    if (parsArg[0] == "rawPRINT"):
+        print(parsArg[1], end="")
+    elif (parsArg[0] == "PRINT"):
+        print(parsArg[1])
+    elif (parsArg[0] == "fPRINT"):
+        print(eval(parsArg[1]), end="")
+    elif (parsArg[0] == "LOOpRINT"):
+        for parsArgIndexEl in range(1, len(parsArg)):
+            print(parsArg[parsArgIndexEl], end="")
+    elif (parsArg[0] == "TYPE"):
+        print(f"<OBJECT {type(ast.literal_eval(parsArg[1])).__name__}>", end="")
+    elif (parsArg[0] == "INPU"):
+        exec(f"{str(parsArg[2])}=input('{parsArg[1]}')")
+    elif (parsArg[0] == "showVAR"):
+        print(eval(parsArg[1]), end="")
+    elif (parsArg[0] == "take"):
+        exec(f"{parsArg[1]} = {parsArg[2]}")
+    elif (parsArg[0] == "@ALLOW"):
+        allowed_module[str(parsArg[1])] = 1
+    elif (parsArg[0] == "END"):
         exit()
     
     # BUILT-IN module
     # FILE module
-    elif (i[0][0:5]=="FILE."):
-        if (allowed_module["FILE"]==1):
-            if (i[0][5:]=="ACCESS"):
-                exec(f"{i[3]} = open('{i[1]}', '{i[2].lower()}')")
-            elif (i[0][5:]=="CLOSE "):
-                exec(f"{i[1]}.close()")
-            elif (i[0][5:]=="CREATE "):
-                exec(f"{i[2]} = open('{i[1]}', 'x')")
-            elif (i[0][5:]=="DELETE "):
-                exec(f"os.remove({i[1]}.name)")        
-            elif (i[0][5:]=="WRITE "):
-                exec(f"{i[1]}.write({i[2]})")    
+    elif (parsArg[0][0:5] == "FILE."):
+        if (allowed_module["FILE"] == 1):
+            if (parsArg[0][5:] == "ACCESS"):
+                exec(f"{parsArg[3]} = open('{parsArg[1]}', '{parsArg[2].lower()}')")
+            elif (parsArg[0][5:] == "CLOSE "):
+                exec(f"{parsArg[1]}.close()")
+            elif (parsArg[0][5:] == "CREATE "):
+                exec(f"{parsArg[2]} = open('{parsArg[1]}', 'x')")
+            elif (parsArg[0][5:] == "DELETE "):
+                exec(f"os.remove({parsArg[1]}.name)")        
+            elif (parsArg[0][5:] == "WRITE "):
+                exec(f"{parsArg[1]}.write({parsArg[2]})")
