6d231cafb2acbc4044e5ab1cac7adb844a8813f8
Dibyojit Datta <dattadibyojit@gmail.com>
2024-09-21 16:13:51 +0530

    Add Execution Cursor
    Added a Execution Cursor that helps to stop the execution of code using BREAK, and again start it using CONTINUE.

--- compile.py
+++ compile.py
@@ -2,6 +2,7 @@ import sys
 import pdb
 import ast
 import os
+import libraries.excsr as excsr
 
 file_to_compile = open(sys.argv[1], "r+")
 
@@ -16,59 +17,69 @@ PROPS = {
     "Author": 0,
 }
 
+# Execution Cursor
+mainCsr = excsr.ExecutionCursor('MAIN-CURSOR')
+mainCsr.init()
+
 
 for parsArg in ccode:
-    # Splits the syntax into function and arguments
-    parsArg = parsArg.split("-")
-    # Removes leading whitespace
-    if len(parsArg[0]) > 0 and parsArg[0][0] == " ":
-        parsArg[0] = parsArg[0].lstrip()
+    if (mainCsr.isUP()):
+        # Splits the syntax into function and arguments
+        parsArg = parsArg.split("-")
+        # Removes leading whitespace
+        if len(parsArg[0]) > 0 and parsArg[0][0] == " ":
+            parsArg[0] = parsArg[0].lstrip()
 
-    tempParsArg = []
-    for cO in parsArg:
-        tempParsArg.append(cO.strip())
-    parsArg = tempParsArg
+        tempParsArg = []
+        for cO in parsArg:
+            tempParsArg.append(cO.strip())
+        parsArg = tempParsArg
 
-    if (parsArg[0] == "rawPRINT"):
-        print(parsArg[1], end="")
-    elif (parsArg[0] == "PRINT"):
-        print(parsArg[1])
-    elif (parsArg[0] == "fPRINT"):
-        print(eval(parsArg[1]), end="")
-    elif (parsArg[0] == "LOOpRINT"):
-        for parsArgIndexEl in range(1, len(parsArg)):
-            print(parsArg[parsArgIndexEl], end="")
-    elif (parsArg[0] == "TYPE"):
-        print(f"<OBJECT {type(ast.literal_eval(parsArg[1])).__name__}>", end="")
-    elif (parsArg[0] == "INPU"):
-        exec(f"{str(parsArg[2])}=input('{parsArg[1]}')")
-    elif (parsArg[0] == "showVAR"):
-        print(eval(parsArg[1]), end="")
-    elif (parsArg[0] == "take"):
-        exec(f"{parsArg[1]} = {parsArg[2]}")
-    elif (parsArg[0] == "@ALLOW"):
-        allowed_module[str(parsArg[1])] = 1
-    elif (parsArg[0] == "END"):
-        exec("exit()")
-    
-    # BUILT-IN module
+        if (parsArg[0] == "rawPRINT"):
+            print(parsArg[1], end="")
+        elif (parsArg[0] == "PRINT"):
+            print(parsArg[1])
+        elif (parsArg[0] == "fPRINT"):
+            print(eval(parsArg[1]), end="")
+        elif (parsArg[0] == "LOOpRINT"):
+            for parsArgIndexEl in range(1, len(parsArg)):
+                print(parsArg[parsArgIndexEl], end="")
+        elif (parsArg[0] == "TYPE"):
+            print(f"<OBJECT {type(ast.literal_eval(parsArg[1])).__name__}>", end="")
+        elif (parsArg[0] == "INPU"):
+            exec(f"{str(parsArg[2])}=input('{parsArg[1]}')")
+        elif (parsArg[0] == "showVAR"):
+            print(eval(parsArg[1]), end="")
+        elif (parsArg[0] == "take"):
+            exec(f"{parsArg[1]} = {parsArg[2]}")
+        elif (parsArg[0] == "@ALLOW"):
+            allowed_module[str(parsArg[1])] = 1
+        elif (parsArg[0] == "END"):
+            exec("exit()")
 
-    # Program Properies Access
-    elif (parsArg[0] == "#define"):
-        defiPROP = parsArg[1]
-        defiPROPValue = parsArg[2]
-        exec(f"PROPS[{defiPROP}]={defiPROPValue}")
 
-    # FILE module
-    elif (parsArg[0][0:5] == "FILE."):
-        if (allowed_module["FILE"] == 1):
-            if (parsArg[0][5:] == "ACCESS"):
-                exec(f"{parsArg[3]} = open('{parsArg[1]}', '{parsArg[2].lower()}')")
-            elif (parsArg[0][5:] == "CLOSE "):
-                exec(f"{parsArg[1]}.close()")
-            elif (parsArg[0][5:] == "CREATE "):
-                exec(f"{parsArg[2]} = open('{parsArg[1]}', 'x')")
-            elif (parsArg[0][5:] == "DELETE "):
-                exec(f"os.remove({parsArg[1]}.name)")        
-            elif (parsArg[0][5:] == "WRITE "):
-                exec(f"{parsArg[1]}.write({parsArg[2]})")
+        # CONDITIONAL BLOCKING
+        elif (parsArg[0]=="BREAK"):
+            mainCsr.makeDOWN()
+        elif (parsArg[0]=="CONTINUE"):
+            mainCsr.makeDOWN()
+
+        # Program Properies Access
+        elif (parsArg[0] == "#define"):
+            defiPROP = parsArg[1]
+            defiPROPValue = parsArg[2]
+            exec(f"PROPS[{defiPROP}]={defiPROPValue}")
+
+        # FILE module
+        elif (parsArg[0][0:5] == "FILE."):
+            if (allowed_module["FILE"] == 1):
+                if (parsArg[0][5:] == "ACCESS"):
+                    exec(f"{parsArg[3]} = open('{parsArg[1]}', '{parsArg[2].lower()}')")
+                elif (parsArg[0][5:] == "CLOSE "):
+                    exec(f"{parsArg[1]}.close()")
+                elif (parsArg[0][5:] == "CREATE "):
+                    exec(f"{parsArg[2]} = open('{parsArg[1]}', 'x')")
+                elif (parsArg[0][5:] == "DELETE "):
+                    exec(f"os.remove({parsArg[1]}.name)")        
+                elif (parsArg[0][5:] == "WRITE "):
+                    exec(f"{parsArg[1]}.write({parsArg[2]})")
---
+++ libraries/excsr.py
@@ -0,0 +1,13 @@
+class ExecutionCursor():
+    value = 0
+    # The cursors help in exexution of a certain function.
+    def __init__(self, name):
+        self.name = name
+    def init(self):
+        self.value = True
+    def makeUP(self):
+        self.value = True
+    def makeDOWN(self):
+        self.value = False
+    def isUP(self):
+        return self.value
